a0aec4d339b1354d9d56b41317b2de44b8b71784
R2u1s <Budz@yandex.ru>
2024-08-07 22:31:55 +0300

    feat: initial commit

---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from "./app.module.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className={styles.app}>
+        hello
+      </header>
+    </div>
+  );
+}
+
+export default App;
